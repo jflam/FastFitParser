46069bccb30626e7a5e20b53b5fe04b459848144
John Lam <jlam@iunknown.com>
2014-12-23 17:28:16 -0800

    Checking in a working CRC16 implementation (all of the code is in the test. Refactoring time

--- src/FastFitParser.Tests/FastCrcTests.cs
+++ src/FastFitParser.Tests/FastCrcTests.cs
@@ -63,5 +63,44 @@ namespace FastFitParser.Tests
 
             Assert.IsTrue(true);
         }
+
+        [TestMethod]
+        public void TestSimpleString2()
+        {
+            string source = "123456789";
+            ushort expectedCRC = 0xBB3D; // CRC16
+
+            FastCRC.InitializeCrc16();
+
+            ushort crc = 0;
+            ushort[,] table = FastCRC.CRC16Table;
+            byte[] data = System.Text.Encoding.UTF8.GetBytes(source);
+            using (var stream = new MemoryStream(data))
+            {
+                using (var reader = new BinaryReader(stream))
+                {
+                    ulong n = reader.ReadUInt64();
+                    crc = (ushort)(table[7, (int)(n & 0xff) ^ ((crc >> 8) & 0xff)] ^
+                                   table[6, (int)((n >> 8) & 0xff) ^ (crc & 0xff)] ^
+                                   table[5, (n >> 16) & 0xff] ^
+                                   table[4, (n >> 24) & 0xff] ^
+                                   table[3, (n >> 32) & 0xff] ^
+                                   table[2, (n >> 40) & 0xff] ^
+                                   table[1, (n >> 48) & 0xff] ^
+                                   table[0, (n >> 56)]);
+
+                    // process remaining bytes
+                    for (int i = 0; i < 1; i++)
+                    {
+                        byte next = reader.ReadByte();
+                        crc = (ushort)(table[0, (crc ^ next) & 0xff] ^ (crc >> 8));
+                    }
+
+                    Assert.AreEqual(expectedCRC, crc);
+                }
+            }
+
+            Assert.IsTrue(true);
+        }
     }
 }
