25d9281cf652a982de9ca4a189816e02c5bc60bd
John Lam <jlam@iunknown.com>
2015-01-04 21:54:40 -0800

    Added Activity, FileId, FileCreator, and Session record definitions

--- src/FastFitParser.Core/Definitions.cs
+++ src/FastFitParser.Core/Definitions.cs
@@ -233,7 +233,143 @@ namespace FastFitParser.Core
         public static readonly FieldDef TimeStamp = Definitions.DefineField(253, "TimeStamp");
     }
 
-    // TODO: Session, Activity, FileId, FileCreator
+    public static class ActivityDef
+    {
+        public static FieldDefinitions Definitions = new FieldDefinitions();
+
+        public static readonly FieldDef TotalTimerTime = Definitions.DefineField(0, "TotalTimerTime");
+        public static readonly FieldDef NumSessions = Definitions.DefineField(1, "NumSessions");
+        public static readonly FieldDef Type = Definitions.DefineField(2, "Type", true, false);
+        public static readonly FieldDef Event = Definitions.DefineField(3, "Event", true, false);
+        public static readonly FieldDef EventType = Definitions.DefineField(4, "EventType", true, false);
+        public static readonly FieldDef LocalTimestamp = Definitions.DefineField(5, "LocalTimestamp");
+        public static readonly FieldDef EventGroup = Definitions.DefineField(6, "EventGroup");
+        public static readonly FieldDef TimeStamp = Definitions.DefineField(253, "TimeStamp");
+    }
+
+    public static class FileIdDef
+    {
+        public static FieldDefinitions Definitions = new FieldDefinitions();
+
+        public static readonly FieldDef Type = Definitions.DefineField(0, "Type", true, false);
+        public static readonly FieldDef Manufacturer = Definitions.DefineField(1, "Manufacturer");
+        public static readonly FieldDef Product = Definitions.DefineField(2, "Product");
+        public static readonly FieldDef GarminProduct = Definitions.DefineField(2, "GarminProduct"); // alias
+        public static readonly FieldDef SerialNumber = Definitions.DefineField(3, "SerialNumber");
+        public static readonly FieldDef TimeCreated = Definitions.DefineField(4, "TimeCreated");
+        public static readonly FieldDef Number = Definitions.DefineField(5, "Number");
+    }
+
+    public static class FileCreatorDef
+    {
+        public static FieldDefinitions Definitions = new FieldDefinitions();
+
+        public static readonly FieldDef SoftwareVersion = Definitions.DefineField(0, "SoftwareVersion");
+        public static readonly FieldDef HardwareVersion = Definitions.DefineField(1, "HardwareVersion");
+    }
+
+    public static class SessionDef
+    {
+        public static FieldDefinitions Definitions = new FieldDefinitions();
+
+        public static readonly FieldDef Event = Definitions.DefineField(0, "Event", true, false);
+        public static readonly FieldDef EventType = Definitions.DefineField(1, "EventType", true, false);
+        public static readonly FieldDef StartTime = Definitions.DefineField(2, "StartTime");
+        public static readonly FieldDef StartPositionLat = Definitions.DefineField(3, "StartPositionLat");
+        public static readonly FieldDef StartPositionLong = Definitions.DefineField(4, "StartPositionLong");
+        public static readonly FieldDef Sport = Definitions.DefineField(5, "Sport", true, false);
+        public static readonly FieldDef SubSport = Definitions.DefineField(6, "SubSport", true, false);
+        public static readonly FieldDef TotalElapsedTime = Definitions.DefineField(7, "TotalElapsedTime");
+        public static readonly FieldDef TotalTimerTime = Definitions.DefineField(8, "TotalTimerTime");
+        public static readonly FieldDef TotalDistance = Definitions.DefineField(9, "TotalDistance");
+        public static readonly FieldDef TotalCycles = Definitions.DefineField(10, "TotalCycles");
+        public static readonly FieldDef TotalStrides = Definitions.DefineField(10, "TotalStrides"); // alias
+        public static readonly FieldDef TotalCalories = Definitions.DefineField(11, "TotalCalories");
+        public static readonly FieldDef TotalFatCalories = Definitions.DefineField(13, "TotalFatCalories");
+        public static readonly FieldDef AvgSpeed = Definitions.DefineField(14, "AvgSpeed");
+        public static readonly FieldDef MaxSpeed = Definitions.DefineField(15, "MaxSpeed");
+        public static readonly FieldDef AvgHeartRate = Definitions.DefineField(16, "AvgHeartRate");
+        public static readonly FieldDef MaxHeartRate = Definitions.DefineField(17, "MaxHeartRate");
+        public static readonly FieldDef AvgCadence = Definitions.DefineField(18, "AvgCadence");
+        public static readonly FieldDef AvgRunningCadence = Definitions.DefineField(18, "AvgRunningCadence"); // alias
+        public static readonly FieldDef MaxCadence = Definitions.DefineField(19, "MaxCadence");
+        public static readonly FieldDef MaxRunningCadence = Definitions.DefineField(19, "MaxRunningCadence"); // alias
+        public static readonly FieldDef AvgPower = Definitions.DefineField(20, "AvgPower");
+        public static readonly FieldDef MaxPower = Definitions.DefineField(21, "MaxPower");
+        public static readonly FieldDef TotalAscent = Definitions.DefineField(22, "TotalAscent");
+        public static readonly FieldDef TotalDescent = Definitions.DefineField(23, "TotalDescent");
+        public static readonly FieldDef TotalTrainingEffect = Definitions.DefineField(24, "TotalTrainingEffect");
+        public static readonly FieldDef FirstLapIndex = Definitions.DefineField(25, "FirstLapIndex");
+        public static readonly FieldDef NumLaps = Definitions.DefineField(26, "NumLaps");
+        public static readonly FieldDef EventGroup = Definitions.DefineField(27, "EventGroup");
+        public static readonly FieldDef Trigger = Definitions.DefineField(28, "Trigger", true, false);
+        public static readonly FieldDef NecLat = Definitions.DefineField(29, "NecLat");
+        public static readonly FieldDef NecLong = Definitions.DefineField(30, "NecLong");
+        public static readonly FieldDef SwcLat = Definitions.DefineField(31, "SwcLat");
+        public static readonly FieldDef SwcLong = Definitions.DefineField(32, "SwcLong");
+        public static readonly FieldDef NormalizedPower = Definitions.DefineField(34, "NormalizedPower");
+        public static readonly FieldDef TrainingStressScore = Definitions.DefineField(35, "TrainingStressScore");
+        public static readonly FieldDef IntensityFactor = Definitions.DefineField(36, "IntensityFactor");
+        public static readonly FieldDef LeftRightBalance = Definitions.DefineField(37, "LeftRightBalance");
+        public static readonly FieldDef AvgStrokeCount = Definitions.DefineField(41, "AvgStrokeCount");
+        public static readonly FieldDef AvgStrokeDistance = Definitions.DefineField(42, "AvgStrokeDistance");
+        public static readonly FieldDef SwimStroke = Definitions.DefineField(43, "SwimStroke", true, false);
+        public static readonly FieldDef PoolLength = Definitions.DefineField(44, "PoolLength");
+        public static readonly FieldDef PoolLengthUnit = Definitions.DefineField(46, "PoolLengthUnit", true, false);
+        public static readonly FieldDef NumActiveLengths = Definitions.DefineField(47, "NumActiveLengths");
+        public static readonly FieldDef TotalWork = Definitions.DefineField(48, "TotalWork");
+        public static readonly FieldDef AvgAltitude = Definitions.DefineField(49, "AvgAltitude");
+        public static readonly FieldDef MaxAltitude = Definitions.DefineField(50, "MaxAltitude");
+        public static readonly FieldDef GpsAccuracy = Definitions.DefineField(51, "GpsAccuracy");
+        public static readonly FieldDef AvgGrade = Definitions.DefineField(52, "AvgGrade");
+        public static readonly FieldDef AvgPosGrade = Definitions.DefineField(53, "AvgPosGrade");
+        public static readonly FieldDef AvgNegGrade = Definitions.DefineField(54, "AvgNegGrade");
+        public static readonly FieldDef MaxPosGrade = Definitions.DefineField(55, "MaxPosGrade");
+        public static readonly FieldDef MaxNegGrade = Definitions.DefineField(56, "MaxNegGrade");
+        public static readonly FieldDef AvgTemperature = Definitions.DefineField(57, "AvgTemperature");
+        public static readonly FieldDef MaxTemperature = Definitions.DefineField(58, "MaxTemperature");
+        public static readonly FieldDef TotalMovingTime = Definitions.DefineField(59, "TotalMovingTime");
+        public static readonly FieldDef AvgPosVerticalSpeed = Definitions.DefineField(60, "AvgPosVerticalSpeed");
+        public static readonly FieldDef AvgNegVerticalSpeed = Definitions.DefineField(61, "AvgNegVerticalSpeed");
+        public static readonly FieldDef MaxPosVerticalSpeed = Definitions.DefineField(62, "MaxPosVerticalSpeed");
+        public static readonly FieldDef MaxNegVerticalSpeed = Definitions.DefineField(63, "MaxNegVerticalSpeed");
+        public static readonly FieldDef MinHeartRate = Definitions.DefineField(64, "MinHeartRate");
+        public static readonly FieldDef TimeInHrZone = Definitions.DefineField(65, "TimeInHrZone", false, true);
+        public static readonly FieldDef TimeInSpeedZone = Definitions.DefineField(66, "TimeInSpeedZone", false, true);
+        public static readonly FieldDef TimeInCadenceZone = Definitions.DefineField(67, "TimeInCadenceZone", false, true);
+        public static readonly FieldDef TimeInPowerZone = Definitions.DefineField(68, "TimeInPowerZone", false, true);
+        public static readonly FieldDef AvgLapTime = Definitions.DefineField(69, "AvgLapTime");
+        public static readonly FieldDef BestLapIndex = Definitions.DefineField(70, "BestLapIndex");
+        public static readonly FieldDef MinAltitude = Definitions.DefineField(71, "MinAltitude");
+        public static readonly FieldDef PlayerScore = Definitions.DefineField(82, "PlayerScore");
+        public static readonly FieldDef OpponentScore = Definitions.DefineField(83, "OpponentScore");
+        public static readonly FieldDef OpponentName = Definitions.DefineField(84, "OpponentName", false, true);  // this is a new type - byte array
+        public static readonly FieldDef StrokeCount = Definitions.DefineField(85, "StrokeCount", false, true);
+        public static readonly FieldDef ZoneCount = Definitions.DefineField(86, "ZoneCount", false, true);
+        public static readonly FieldDef MaxBallSpeed = Definitions.DefineField(87, "MaxBallSpeed");
+        public static readonly FieldDef AvgBallSpeed = Definitions.DefineField(88, "AvgBallSpeed");
+        public static readonly FieldDef AvgVerticalOscillation = Definitions.DefineField(89, "AvgVerticalOscillation");
+        public static readonly FieldDef AvgStanceTimePercent = Definitions.DefineField(90, "AvgStanceTimePercent");
+        public static readonly FieldDef AvgStanceTime = Definitions.DefineField(91, "AvgStanceTime");
+        public static readonly FieldDef AvgFractionalCadence = Definitions.DefineField(92, "AvgFractionalCadence");
+        public static readonly FieldDef MaxFractionalCadence = Definitions.DefineField(93, "MaxFractionalCadence");
+        public static readonly FieldDef TotalFractionalCycles = Definitions.DefineField(94, "TotalFractionalCycles");
+        public static readonly FieldDef AvgTotalHemoglobinConc = Definitions.DefineField(95, "AvgTotalHemoglobinConc", false, true);
+        public static readonly FieldDef MinTotalHemoglobinConc = Definitions.DefineField(96, "MinTotalHemoglobinConc", false, true);
+        public static readonly FieldDef MaxTotalHemoglobinConc = Definitions.DefineField(97, "MaxTotalHemoglobinConc", false, true);
+        public static readonly FieldDef AvgSaturatedHemoglobinPercent = Definitions.DefineField(98, "AvgSaturatedHemoglobinPercent", false, true);
+        public static readonly FieldDef MinSaturatedHemoglobinPercent = Definitions.DefineField(99, "MinSaturatedHemoglobinPercent", false, true);
+        public static readonly FieldDef MaxSaturatedHemoglobinPercent = Definitions.DefineField(100, "MaxSaturatedHemoglobinPercent", false, true);
+        public static readonly FieldDef AvgLeftTorqueEffectiveness = Definitions.DefineField(101, "AvgLeftTorqueEffectiveness");
+        public static readonly FieldDef AvgRightTorqueEffectiveness = Definitions.DefineField(102, "AvgRightTorqueEffectiveness");
+        public static readonly FieldDef AvgLeftPedalSmoothness = Definitions.DefineField(103, "AvgLeftPedalSmoothness");
+        public static readonly FieldDef AvgRightPedalSmoothness = Definitions.DefineField(104, "AvgRightPedalSmoothness");
+        public static readonly FieldDef AvgCombinedPedalSmoothness = Definitions.DefineField(105, "AvgCombinedPedalSmoothness");
+
+        public static readonly FieldDef TimeStamp = Definitions.DefineField(253, "TimeStamp");
+        public static readonly FieldDef MessageIndex = Definitions.DefineField(254, "MessageIndex");
+    }
+
 
     public enum Event : byte
     {
