9989425cc3eb42c9d4a42995b5d87a6e2559a798
John Lam <jlam@iunknown.com>
2015-01-05 21:56:54 -0800

    updating version #s

--- src/FastFitParser.Core/Properties/AssemblyInfo.cs
+++ src/FastFitParser.Core/Properties/AssemblyInfo.cs
@@ -3,9 +3,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("FastFitParser.Core")]
 [assembly: AssemblyDescription("High performance FIT file parser packaged as a PCL.")]
 [assembly: AssemblyConfiguration("")]
@@ -16,15 +13,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.1.0")]
-[assembly: AssemblyFileVersion("0.2.1.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
